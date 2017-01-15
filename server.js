0bf25065d569b54b4fbdb70c4d785d57dfb8db44
Aaron Washburn <aaron@it-kutch.com>
2017-01-15 17:54:09 -0500

    Add delete and put endpoints

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-  var body = req.body
+  var body = _.pick(req.body, 'description', 'completed');
 
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
     return res.status(400).send();
@@ -41,9 +41,50 @@ app.post('/todos', function (req, res) {
 
   body.description = body.description.trim();
   body.id = todoNextId++;
-  body = _.pick(body, 'description', 'completed')
+
   todos.push(body);
   res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (matchedTodo) {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send({"error": "No todo found with that id"});
+  }
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  } 
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  _.extend(matchedTodo, validAttributes);
+
+  res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
