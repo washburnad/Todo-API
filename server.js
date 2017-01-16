161d893165272d42cda2d908435d2f33370eb117
Aaron Washburn <aaron@it-kutch.com>
2017-01-16 15:22:51 -0500

    Add index page filter

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  console.log(filteredTodos);
+
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {'completed': true})
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {'completed': false})
+  }
+  
+  res.json(filteredTodos);
 });
 
 
