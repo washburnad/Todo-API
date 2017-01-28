1a331c0663bea4db69fb45518956210f876f8ecb
Aaron Washburn <aaron@it-kutch.com>
2017-01-28 17:14:03 -0500

    Add todo model and post todo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -62,15 +63,24 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send();
-  }
+  // call create on db.todo
+  // respond with 200 and todo
+  // or respond with e (res.status(400).json(e))
 
-  body.description = body.description.trim();
-  body.id = todoNextId++;
+  db.todo.create(body).then(function (todo) {
+    res.json(todo.toJSON());
+  }).catch(function (e) {
+    res.status(400).json(e);
+  });
+  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  //   return res.status(400).send();
+  // }
 
-  todos.push(body);
-  res.json(body);
+  // body.description = body.description.trim();
+  // body.id = todoNextId++;
+
+  // todos.push(body);
+  // res.json(body);
 });
 
 // DELETE /todos/:id
@@ -120,6 +130,8 @@ app.put('/todos/:id', function(req, res) {
   res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-  console.log('Express listening on port ' + PORT + '.');
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function() {
+    console.log('Express listening on port ' + PORT + '.');
+  });
 });
