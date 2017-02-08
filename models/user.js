cb07bc57848f7557158107300fe74a25cabaef2c
Aaron Washburn <aaron@it-kutch.com>
2017-02-08 18:09:57 -0500

    Add user model and salt passwords

---
+++ models/user.js
@@ -0,0 +1,50 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                len: [7, 100]
+            },
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword)
+            }
+        }  
+    }, {
+        hooks: {
+            beforeValidate: function(user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function () {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'created_at', 'updated_at')
+            }
+        }
+    });
+}
