b4aa4e53fe465d12e18d754303406944641f2f3f
Aaron Washburn <aaron@it-kutch.com>
2017-02-12 15:35:39 -0500

    Add user login and authentication

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -40,11 +40,34 @@ module.exports = function(sequelize, DataTypes) {
                 }
             }
         },
+        classMethods: {
+            authenticate: function(body) {
+                return new Promise( function (resolve, reject) {
+                    if ((typeof body.email !== 'string') || (typeof body.password !== 'string')) {
+                        return reject();
+                    } else {
+                        user.findOne({ 
+                            where: {email: body.email } 
+                        }).then(function (user) {
+                            if ((user) && bcrypt.compareSync(body.password, user.get('password_hash'))){
+                                return resolve(user);
+                            } else {
+                                return reject();;
+                            }
+                        }).catch(function (e) {
+                            reject();
+                        });
+                    }
+                })
+            }
+        },
         instanceMethods: {
             toPublicJSON: function () {
                 var json = this.toJSON();
-                return _.pick(json, 'id', 'email', 'created_at', 'updated_at')
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt')
             }
         }
     });
+
+    return user;
 }
