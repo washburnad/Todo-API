b762451efdc8163eb703abf20c3540bded708a4e
Aaron Washburn <aaron@it-kutch.com>
2017-02-18 14:04:10 -0500

    Scope todos to user

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -20,45 +20,37 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
     // force: true
 }).then(function() {
     console.log('Everything is synced');
 
-    Todo.findById(1).then(function (todo) {
-        if (todo) {
-            console.log(todo.toJSON());
-        } else {
-            console.log('Item was not found');
-        }
-    });
-
-
-
-    // Todo.create({
-    //     description: 'Take out trash'
-    // }).then(function (todo) {
+    // User.create({
+    //     email: 'example@example.com'
+    // }).then(function () {
     //     return Todo.create({
-    //         description: 'Clean office'
+    //         description: 'Clean yard'
     //     });
-    // }).then(function() {
-    //     // return Todo.findById(1)
-    //     return Todo.findAll({
-    //         where: {
-    //             description: {
-    //                 $like: '%Office%'
-    //             }
-    //         }
-    //     })
-    // }).then(function (todos) {
-    //     if (todos) {
-    //         todos.forEach(function (todo) {
-    //             console.log(todo.toJSON());                
-    //         });
-    //     } else {
-    //         console.log('no todos found');
-    //     }
-    // }).catch(function(e){
-    //     console.log(e);
-    // });
+    // }).then(function (todo) {
+    //     User.findById(1).then(function (user) {
+    //         user.addTodo(todo);
+    //     });
+    // })
+
+    User.findById(1).then(function (user) {
+        user.getTodos({ 
+            where: { completed: false } 
+        }).then(function (todos) {
+            todos.forEach(function (todo) {
+                console.log(todo.toJSON());
+            })
+        })
+    })
 });
--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 // GET /todos?completed=true&q=search
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if (query.hasOwnProperty('completed')) {
     if (query.completed === 'true') {
@@ -51,7 +53,10 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
     
-  db.todo.findById(todoId).then(function(todo){
+  db.todo.findOne({
+    where: { id: todoId,
+             userId: req.user.id }
+  }).then(function(todo){
     if (!!todo) {
       res.json(todo.toJSON());
     } else {
@@ -67,7 +72,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
   db.todo.create(body).then(function (todo) {
-    res.json(todo.toJSON());
+    req.user.addTodo(todo).then(function () {
+      return todo.reload();
+    }).then(function () {
+      res.json(todo.toJSON());
+    })
   }).catch(function (e) {
     res.status(400).json(e);
   });
@@ -77,9 +86,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   
+  console.log('user is ', req.user)
   db.todo.destroy({
     where: {
-      id: todoId
+      id: todoId,
+      userId: req.user.id
     }
   }).then(function(rowsDeleted) {
     if (rowsDeleted === 0) {
@@ -108,7 +119,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     attributes.description = body.description;
   }
 
-  db.todo.findById(todoId).then(function (todo) {
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function (todo) {
     if (todo) {
       todo.update(attributes).then(function (todo) {
         res.json(todo.toJSON());
@@ -151,7 +167,7 @@ app.post('/users/login', function(req, res) {
   });
 });
 
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync({force: false}).then(function () {
   app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '.');
   });
